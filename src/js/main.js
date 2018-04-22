8b900e145fd59c88f35398c9bc2067c76e5d73f7
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2018-04-22 14:12:48 -0500

    begin adding firebase auth to project

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
 const $ = require('jQuery');
 const bootstrap = require('bootstrap')
+const firebase = require('firebase')
+
+var config = {
+    apiKey: "AIzaSyDByoKRhcKOCVY-9YTPMGcSTwXJyFm4LXM",
+    authDomain: "tmnt-rpg.firebaseapp.com",
+    databaseURL: "https://tmnt-rpg.firebaseio.com",
+    projectId: "tmnt-rpg",
+    storageBucket: "tmnt-rpg.appspot.com",
+    messagingSenderId: "719978376686"
+};
+
+firebase.initializeApp(config);
+
+$('#btn-login').bind('click', function (ev) {
+    ev.preventDefault();
+    console.log('click')
+});
+
+
